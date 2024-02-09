598a178fcb6ab21c501aca6802db1fd35a235b3f
Martin Burian <burian.martin@icloud.com>
2024-02-10 00:07:10 +0100

    Mini edit

--- Personal/C++/LeetCode/ValidParentheses.cpp
+++ Personal/C++/LeetCode/ValidParentheses.cpp
@@ -6,7 +6,7 @@ bool isValid(std::string s);
 int main(int argc, char const *argv[])
 {
 
-    std::string s = "[{()}]";
+    std::string s = "{[()]}";
 
     std::cout << (isValid(s) ? "Valid" : "Invalid");
     return 0;
@@ -17,23 +17,16 @@ bool isValid(std::string s)
 
     std::vector<int> check;
 
-    int normal = 0, square = 0, curly = 0;
-
-    int last = 0;
-
     for (int i = 0; i < s.length(); i++)
     {
 
         switch (int(s[i]))
         {
         case 40:
-            normal++;
             check.push_back(40);
-            last = 40;
             break;
 
         case 41:
-            normal--;
             if (!check.empty() && check[check.size() - 1] == 40)
                 check.pop_back();
             else
@@ -42,13 +35,10 @@ bool isValid(std::string s)
             break;
 
         case 91:
-            square++;
             check.push_back(91);
-            last = 91;
             break;
 
         case 93:
-            square--;
             if (!check.empty() && check[check.size() - 1] == 91)
                 check.pop_back();
             else
@@ -57,13 +47,10 @@ bool isValid(std::string s)
             break;
 
         case 123:
-            curly++;
             check.push_back(123);
-            last = 123;
             break;
 
         case 125:
-            curly--;
             if (!check.empty() && check[check.size() - 1] == 123)
                 check.pop_back();
             else
@@ -78,4 +65,6 @@ bool isValid(std::string s)
 
     if (check.size() == 0)
         return true;
+    else
+        return false;
 }
